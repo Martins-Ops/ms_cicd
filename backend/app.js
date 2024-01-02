4a4955078bbf13f6df2519b567615ff7b31b924c
oluwafemi <oluwafemimartins1234@gmail.com>
2024-01-02 17:06:13 +0100

    add file for backend app

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+
+app.get('/', (_, res) => {
+  res.send('TALENT PLUS');
+});
+
+app.use((_, res, next) => {
+  res.send('INVALID ROUTE');
+  next();
+});
+
+app.listen(PORT);
+console.log(`Server started on port ${PORT}`);
